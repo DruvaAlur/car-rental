7050a93cb64b04aaa53c7ff8cd1f761e5a262b29
Druva Alur <druva.alur@gmail.com>
2023-12-17 15:41:26 +0530

    adding components

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageParentComponent } from './main-page-parent/main-page-parent.component';
 import { CarcardsComponent } from './subComponents/carcards/carcards.component';
+import { LocationComponent } from './subComponents/location/location.component';
+import { ReverseComponent } from './subComponents/reverse/reverse.component';
+import { HeaderComponent } from './subComponents/header/header.component';
 
 
 @NgModule({
   declarations: [
     MainPageParentComponent,
-    CarcardsComponent
+    CarcardsComponent,
+    LocationComponent,
+    ReverseComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
