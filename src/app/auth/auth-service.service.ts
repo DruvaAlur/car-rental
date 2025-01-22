6c467524b42b5b2e3fd53c7693614425338cf623
DruvaAlur <druva.alur@gmail.com>
2025-01-22 09:13:13 +0530

    integrating oauth

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import {baseUrl} from '../../../environment'
+import { baseUrl } from '../../../environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthServiceService {
-  loginCompFlag:BehaviorSubject<boolean>=new BehaviorSubject(true);
-  constructor(private http: HttpClient) { }
+  loginCompFlag: BehaviorSubject<boolean> = new BehaviorSubject(true);
+  constructor(private http: HttpClient) {}
 
-  signUp(data:any){
-    return this.http.post(`${baseUrl}auth/signUp`,data)
+  signUp(data: any) {
+    return this.http.post(`${baseUrl}auth/signUp`, data);
   }
 
-  login(data:any){
-    return this.http.post(`${baseUrl}auth/login`,data)
+  login(data: any) {
+    return this.http.post(`${baseUrl}auth/login`, data);
+  }
+  authLogin(data: any) {
+    return this.http.post(`${baseUrl}auth/authLogin`, data);
   }
 }
--- src/app/auth/subComponents/login/login.component.ts
+++ src/app/auth/subComponents/login/login.component.ts
@@ -5,53 +5,61 @@ import {
   GoogleLoginProvider,
   SocialUser,
 } from '@abacritt/angularx-social-login';
-import { AuthServiceService } from '../../auth-service.service';import { trigger, transition, style, animate } from '@angular/animations';
+import { AuthServiceService } from '../../auth-service.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-  loginForm:FormGroup;
+  loginForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
     private socialAuthService: SocialAuthService,
     private authService: AuthServiceService,
-    private router:Router
+    private router: Router
   ) {
-    this.loginForm=this.formBuilder.group({
-      email:this.formBuilder.control('',[Validators.required]),
-      password:this.formBuilder.control('',[Validators.required])
-    })
+    this.loginForm = this.formBuilder.group({
+      email: this.formBuilder.control('', [Validators.required]),
+      password: this.formBuilder.control('', [Validators.required]),
+    });
   }
 
-  ngOnInit(){
-    this.socialAuthService.authState.subscribe((user:any) => {
-      localStorage.setItem('userObject', JSON.stringify(user));
-      console.log(user)
-  });
-  console.log('login child initialized');
+  ngOnInit() {
+    this.socialAuthService.authState
+      .pipe(
+        switchMap((user: any) => {
+          return this.authService.authLogin({ idToken: user.idToken });
+        })
+      )
+      .subscribe((response: any) => {
+        if (response.token) {
+          this.router.navigate(['/dashboard']);
+        }
+      });
   }
-  
+
   loginWithGoogle(): void {
     this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
 
-  signUpUser(){
-    this.authService.loginCompFlag.next(false)
+  signUpUser() {
+    this.authService.loginCompFlag.next(false);
   }
 
-  login(){
-    if(!this.loginForm.valid){
-      return
+  login() {
+    if (!this.loginForm.valid) {
+      return;
     }
-    this.authService.login(this.loginForm.value).subscribe((res:any)=>{
-      if(res.token){
-        this.router.navigate(['/dashboard'])
+    this.authService.login(this.loginForm.value).subscribe((res: any) => {
+      if (res.token) {
+        this.router.navigate(['/dashboard']);
       }
-    })
+    });
   }
 }
