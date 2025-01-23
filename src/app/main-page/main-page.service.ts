689b5d4dc41bd31b95192b3de461ad1fea215524
DruvaAlur <druva.alur@gmail.com>
2025-01-23 08:41:10 +0530

    adding cities list

--- src/app/main-page/main-page.service.ts
+++ src/app/main-page/main-page.service.ts
@@ -1,78 +1,82 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import {DomSanitizer} from '@angular/platform-browser';
-import {baseUrl} from '../../../environment'
+import { DomSanitizer } from '@angular/platform-browser';
+import { baseUrl } from '../../../environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MainPageService {
-  cardContents:BehaviorSubject<any>=new BehaviorSubject([]);
-  carDetails:BehaviorSubject<any>=new BehaviorSubject('')
-  searchedResult:BehaviorSubject<any> = new BehaviorSubject([])
-  selectedCarId:BehaviorSubject<any> = new BehaviorSubject('')
-  dropOff:BehaviorSubject<any> = new BehaviorSubject({})
-  pickUp:BehaviorSubject<any> = new BehaviorSubject({})
-  nominatimUrl:string='https://nominatim.openstreetmap.org/reverse';
-  get carDetail(){
-    return this.carDetails as Observable<any>
+  cardContents: BehaviorSubject<any> = new BehaviorSubject([]);
+  carDetails: BehaviorSubject<any> = new BehaviorSubject('');
+  searchedResult: BehaviorSubject<any> = new BehaviorSubject([]);
+  selectedCarId: BehaviorSubject<any> = new BehaviorSubject('');
+  dropOff: BehaviorSubject<any> = new BehaviorSubject({});
+  pickUp: BehaviorSubject<any> = new BehaviorSubject({});
+  citiesList: BehaviorSubject<any> = new BehaviorSubject([]);
+  nominatimUrl: string = 'https://nominatim.openstreetmap.org/reverse';
+  get carDetail() {
+    return this.carDetails as Observable<any>;
   }
 
-  setCarDetails(carDetails:any){
-    this.carDetails.next(carDetails)
+  setCarDetails(carDetails: any) {
+    this.carDetails.next(carDetails);
   }
 
-  get searchedResults(){
-    return this.searchedResult as Observable<any>
+  get searchedResults() {
+    return this.searchedResult as Observable<any>;
   }
 
-  searchResults(res:any[]){
+  searchResults(res: any[]) {
     this.searchedResult.next(res);
   }
 
-  get cardContent(){
-    return this.cardContents as Observable<any>
+  get cardContent() {
+    return this.cardContents as Observable<any>;
   }
 
-  constructor(private http: HttpClient,private sanitizer:DomSanitizer) { }
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
   getAllCards() {
-    this.http.get(`${baseUrl}cars/getAllCars`).subscribe((res)=>{
-      this.cardContents.next(res);
-    },(err)=>{
-      console.log(err)
-    })
+    this.http.get(`${baseUrl}cars/getAllCars`).subscribe(
+      (res) => {
+        this.cardContents.next(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
-  _arraybuffertobase64( buffer:any ) {
+  _arraybuffertobase64(buffer: any) {
     var binary = '';
-    var bytes = new Uint8Array( buffer );
+    var bytes = new Uint8Array(buffer);
     var len = bytes.byteLength;
     for (var i = 0; i < len; i++) {
-       binary += String.fromCharCode( bytes[ i ] );
+      binary += String.fromCharCode(bytes[i]);
     }
     return binary;
   }
 
-  sanitize( url:string ) {
+  sanitize(url: string) {
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
 
-  getSearchSuggestions(value:string): Observable<any>{
-    return this.http.get(`${baseUrl}cars/autoComplete?searchQuery=${value}`)
+  getSearchSuggestions(value: string): Observable<any> {
+    return this.http.get(`${baseUrl}cars/autoComplete?searchQuery=${value}`);
   }
 
-  getSearchResults(value:string): Observable<any>{
-    return this.http.get(`${baseUrl}cars/search?searchQuery=${value}`)
+  getSearchResults(value: string): Observable<any> {
+    return this.http.get(`${baseUrl}cars/search?searchQuery=${value}`);
   }
 
-  getCarById(id:string){
-    return this.http.get(`${baseUrl}cars/getCarById?searchQuery=${id}`)
+  getCarById(id: string) {
+    return this.http.get(`${baseUrl}cars/getCarById?searchQuery=${id}`);
   }
 
-  redirectToPayment(id:string){
-    return this.http.post(`${baseUrl}cars/checkout`,{carid:id})
+  redirectToPayment(id: string) {
+    return this.http.post(`${baseUrl}cars/checkout`, { carid: id });
   }
 
   getCityAndState(latitude: number, longitude: number): Observable<any> {
@@ -80,13 +84,23 @@ export class MainPageService {
     return this.http.get(url);
   }
 
-  getStoreLocation(request:any){
-    return this.http.post(`${baseUrl}cars/getStoreLocation`,request)
+  getStoreLocation(request: any) {
+    return this.http.post(`${baseUrl}cars/getStoreLocation`, request);
   }
 
-  checkStatus(){
-    return this.http.get(`${baseUrl}auth/checkStatus`,{
+  checkStatus() {
+    return this.http.get(`${baseUrl}auth/checkStatus`, {
       withCredentials: true,
-    })
+    });
+  }
+
+  getCitiesList() {
+    console.log(this.citiesList.value);
+    if (this.citiesList.value.length != 0) {
+      return;
+    }
+    this.http.get(`${baseUrl}cars/getCityList`).subscribe((response) => {
+      this.citiesList.next(response);
+    });
   }
 }
--- src/app/main-page/subComponents/location/location.component.ts
+++ src/app/main-page/subComponents/location/location.component.ts
@@ -1,26 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MainPageService } from '../../main-page.service';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
-  styleUrls: ['./location.component.css']
+  styleUrls: ['./location.component.css'],
 })
 export class LocationComponent {
-  @Input() data:any;
-  @Input() heading:any;
-  locationForm:FormGroup;
-  constructor(){
-    this.locationForm=new FormGroup({
-      location:new FormControl(''),
-      date:new FormControl(''),
-      time:new FormControl('')
-    })
+  @Input() data: any;
+  @Input() heading: any;
+  locationForm: FormGroup;
+  cityList: any[] = [];
+  constructor(private MainPageService: MainPageService) {
+    this.locationForm = new FormGroup({
+      location: new FormControl(''),
+      date: new FormControl(''),
+      time: new FormControl(''),
+    });
   }
-  getFormValue(){
-    return this.locationForm.value
+  getFormValue() {
+    return this.locationForm.value;
   }
-  ngOnInit(){
+  ngOnInit() {
+    this.MainPageService.getCitiesList();
+    this.MainPageService.citiesList.subscribe((cities) => {
+      console.log(cities);
 
+      this.cityList = cities;
+    });
   }
 }
